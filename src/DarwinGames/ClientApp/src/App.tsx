4be73beca599dbd2f17a274b226aea7593616b9a
Pavel Mikheev <donotcodeit@gmail.com>
2021-03-08 15:16:51 +0700

    Create and configure initial dotnet react template.

---
+++ src/DarwinGames/ClientApp/src/App.tsx
@@ -0,0 +1,10 @@
+import { CssBaseline, Container } from "@material-ui/core";
+import React from "react";
+
+export const App = () => {
+    return (
+        <Container component="main" maxWidth="lg">
+            <CssBaseline />
+        </Container>
+    );
+};
