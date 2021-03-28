33f585f1d4f765f2ae77f721ef5c7096a5ee5db5
Pavel Mikheev <donotcodeit@gmail.com>
2021-03-29 05:51:38 +0700

    Add ability to select games.

--- src/DarwinGames/ClientApp/src/App.tsx
+++ src/DarwinGames/ClientApp/src/App.tsx
@@ -1,16 +1,37 @@
-import { CssBaseline, Container } from "@material-ui/core";
-import React from "react";
+import { CssBaseline, Container, AppBar, Toolbar, IconButton, Typography, Button, Box } from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
+import React, { useState } from "react";
 
 import { A15PuzzleLearnGui } from "./world/15puzzle/gui";
+import { SnakeLearnGui } from "./world/snake/gui";
 
 // TODO: Configure linter. Especial: line breaks, extra new lines, named import alphabetical.
 // TODO: Get rid of zombie Node.js processes.
 
 export const App = () => {
+    const [game, setGame] = useState<"15puzzle" | "snake">("15puzzle");
+    const play15puzzle = () => setGame("15puzzle");
+    const playSnake = () => setGame("snake");
+
     return (
         <Container component="main" maxWidth="lg">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu />
+                    </IconButton>
+                    <Typography variant="h6">
+                        Darwin Games
+                    </Typography>
+                    <Box ml={3}>
+                        <Button color="inherit" onClick={play15puzzle}>15 Puzzle</Button>
+                        <Button color="inherit" onClick={playSnake}>Snake</Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
             <CssBaseline />
-            <A15PuzzleLearnGui />
+            {game === "15puzzle" && <A15PuzzleLearnGui />}
+            {game === "snake" && <SnakeLearnGui />}
         </Container>
     );
 };
